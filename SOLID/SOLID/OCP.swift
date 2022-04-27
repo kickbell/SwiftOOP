253a986c16205816faa02cdd34ccd8e592e8cbc7
kickbell <dev.kickbell@gmail.com>
2022-04-27 22:44:58 +0900

    add files

---
+++ SOLID/SOLID/OCP.swift
@@ -0,0 +1,8 @@
+//
+//  OCP.swift
+//  SOLID
+//
+//  Created by jc.kim on 4/27/22.
+//
+
+import Foundation
---
+++ SOLID/SOLID/SRP.swift
@@ -0,0 +1,8 @@
+//
+//  SRP.swift
+//  SOLID
+//
+//  Created by jc.kim on 4/27/22.
+//
+
+import Foundation
